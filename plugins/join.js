8232b7b28be63c18eb3ef0f31a228dc391130cc9
azdinhh <102433984+azdinhh@users.noreply.github.com>
2022-04-06 12:57:14 +0000

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -2,7 +2,7 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text, usedPrefix }) => {
     let [_, code] = text.match(linkRegex) || []
-    if (!code) throw 'Wrong Link'
+    if (!code) throw 'رابط خطا'
     let res = await conn.acceptInvite(code)
     m.reply(`Successfully joined the group ${res.gid}`).then(() => {
         var jumlahHari = 86400000 * 3
@@ -11,9 +11,9 @@ let handler = async (m, { conn, text, usedPrefix }) => {
         else global.db.data.chats[res.gid].expired = now + jumlahHari
     })
     conn.sendButton(res.gid, `
-*${conn.user.name}* is a whatsapp bot built with Nodejs, *${conn.user.name}* invited by @${m.sender.split`@`[0]}
+*${conn.user.name}* هاي انا نيزوكو  , *${conn.user.name}* بدعوة من @${m.sender.split`@`[0]}
     
-type *${usedPrefix}menu* to see a list of commands`.trim(), watermark, 'Menu', `${usedPrefix}?`, 0, { contextInfo: { mentionedJid: [m.sender] } })
+type *${usedPrefix}menu* لرؤية قائمة الأوامر`.trim(), watermark, 'Menu', `${usedPrefix}?`, 0, { contextInfo: { mentionedJid: [m.sender] } })
 }
 handler.help = ['join <chat.whatsapp.com>']
 handler.tags = ['']
